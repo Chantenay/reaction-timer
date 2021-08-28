14fc76a071416726a9e2e6d060af2fb33e525a61
Harriet <vegimitejuice@gmail.com>
2021-08-28 19:36:04 +1000

    done

---
+++ src/components/Results.vue
@@ -0,0 +1,47 @@
+<template>
+    <p>Reaction time: {{score}} ms</p>
+    <p class="rank">{{rank}}</p>
+</template>
+ 
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    }, 
+    mounted() {
+        if(this.score < 250) {
+            this.rank = "Ninja Fingers"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes"
+        } else {
+            this.rank = "Snail Pace..."
+        }
+    }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+</style>
